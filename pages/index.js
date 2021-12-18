1be2cfee24005926b0dce3aefdb289715bdcd07a
Shubbair <hussainsalih.github@gmail.com>
2021-12-18 13:49:10 +0300

    Update Files

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
 const Home = () => {
     return ( 
-        <h1>Hello World!</h1>
+        <>
+        <div className="">
+            <div className="">
+                <p>The world between your hand</p>
+                <p>We are connected the world throw sharing
+                   sceince , persons , places and more ...</p>
+                <br/>
+                <button>Lets Discover ...</button>
+            </div>
+            <div className="">
+                <img src="hero_img.png" />
+            </div>
+        </div>
+        </>
      );
 }
  
