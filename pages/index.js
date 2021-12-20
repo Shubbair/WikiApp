5ea7fe12b34f36c39ca6931f5ee7544e507c5615
Shubbair <hussainsalih.github@gmail.com>
2021-12-20 13:53:42 +0300

    Update files

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
 const Home = () => {
     return ( 
         <>
-        <div className="grid items-center justify-center my-8 sm:grid-cols-2">
+        <div className="grid items-center justify-center my-8 sm:grid-cols-2 md:mx-4 md:my-24">
             <div className="order-2 mx-6 sm:order-1">
-                <p className="text-xl font-medium">The world between your hand</p>
+                <p className="text-xl font-medium md:text-2xl lg:text-3xl xl:text-5xl">The world between your hand</p>
                 <br/>
-                <p className="text-sm font-light">We are connected the world throw sharing
+                <p className="text-sm font-light md:text-md lg:text-xl xl:text-2xl">We are connected the world throw sharing
                    sceince , persons , places and more ...</p>
                 <br/>
-                <button className="p-2 text-sm bg-yellow-400 rounded btn btn-primary">Lets Discover ...</button>
+                <button className="p-2 text-sm bg-yellow-400 rounded btn btn-primary md:text-md lg:text-md lg:p-3">Lets Discover ...</button>
             </div>
             <div className="order-1 mx-6 my-4 sm:order-2">
                 <img src="hero_img.png" />
             </div>
         </div>
         {/* Card Section */}
-        <div className="grid items-center justify-center">
+        <div className="grid items-center justify-center gap-6 mx-6">
             <div className="mx-auto text-xl">Search for everything</div>
-            <div className="grid grid-flow-row sm:grid-cols-3">
+            <div className="grid grid-flow-row gap-8 sm:grid-cols-3">
                 <div className="">
                     <img src="science.png"/>
                     <p>Sciense</p>
